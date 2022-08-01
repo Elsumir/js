92ce4ee888690e5ed3b4dde98b6e2e679df0f53f
Serik <askaroff_serik@mail.ru>
2022-08-01 10:22:43 +0300

    add funshen

--- js/script.js
+++ js/script.js
@@ -1,10 +1,16 @@
 'use strict';
 
-const numberOfFilms = +prompt('Сколько фильмов вы посмотрели?', '');
-const a = prompt('Один из последних просмотренных фильмов?');
-const b = prompt('На сколько оцените его?');
-const c = prompt('Один из последних просмотренных фильмов?');
-const d = prompt('На сколько оцените его?');
+let numberOfFilms;
+
+function start() {
+  numberOfFilms = +prompt('Сколько фильмов вы посмотрели?', '');
+
+  while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
+    numberOfFilms = +prompt('Сколько фильмов вы посмотрели?', '');
+  }
+}
+
+start();
 
 const personalMovieDB = {
   count: numberOfFilms,
@@ -13,5 +19,50 @@ const personalMovieDB = {
   genres: [],
   privat: false
 };
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+
+function rememberMyFilms() {
+  for (let i = 0; i < 2; i++) {
+    const a = prompt('Один из последних просмотренных фильмов?'),
+      b = prompt('На сколько оцените его?');
+
+    if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+      personalMovieDB.movies[a] = b;
+      console.log('hi');
+    } else {
+      console.log('erroe');
+      i--;
+    }
+  }
+}
+// rememberMyFilms();
+
+function detectPersonalLevel() {
+  if (personalMovieDB.count < 10) {
+    alert('Просмотрено довольно мало фильмов');
+  } else if (personalMovieDB.count > 30) {
+    alert('Вы киноман');
+  } else {
+    alert('Вы классический зритель');
+  }
+}
+
+// detectPersonalLevel();
+
+function showMyDB(hiden) {
+  if (!hiden) {
+    console.log(personalMovieDB);
+  }
+}
+
+showMyDB(numberOfFilms.privat);
+
+function writeYourGenres() {
+  for (let i = 1; i < 4; i++) {
+    personalMovieDB.genres[i - 1] = prompt(
+      `"Ваш любимый жанр под номером ${i}"`
+    );
+  }
+}
+writeYourGenres();
+
+console.log(personalMovieDB);
